e92be031159b93ae2a5c4800edd3f8ea4db94027
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-05-23 11:25:48 +0200

    step 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,38 @@
 //Hamburgermenu
-var hamburgerbutton = document.querySelector(".hamburger-button");
-var hamburgermenu = document.querySelector('.main-header__menu-bar-nav');
+function toggleMenu() {
+    var hamburgerButton = document.querySelector(".hamburger-button");
+    var hamburgerMenu = document.querySelector('.main-header__menu-bar-nav');
 
-hamburgerbutton.addEventListener("click", function () {
-    hamburgerbutton.classList.toggle("hamburger-button--is-open")
-    hamburgermenu.classList.toggle("main-header__menu-bar-nav--is-open")
-});
+    hamburgerButton.addEventListener("click", function () {
+        hamburgerButton.classList.toggle("hamburger-button--is-open")
+        hamburgerMenu.classList.toggle("main-header__menu-bar-nav--is-open")
+    });
+}
+toggleMenu()
+
+//Slideshow
+function toggleSlide() {
+    const slides = document.querySelectorAll('[data-slide]');
+    const interactionElementNext = document.querySelector('[data-nav-next-slide]');
+    const interactionElementPrevious = document.querySelector('[data-nav-previous-slide]');
+
+    let sichtbaresBild = 0
+    slides[sichtbaresBild].classList.toggle('slide-show__slide--visible')
+
+    interactionElementNext.addEventListener('click', () => { slidewechseln(sichtbaresBild + 1) });
+    interactionElementPrevious.addEventListener('click', () => { slidewechseln(sichtbaresBild - 1) });
+
+    function slidewechseln(neuesBild) {
+        slides[sichtbaresBild].classList.toggle('slide-show__slide--visible')
+
+        if (neuesBild < 0) {
+            neuesBild = slides.length - 1
+        }
+        if (neuesBild >= slides.length) {
+            neuesBild = 0
+        }
+        slides[neuesBild].classList.toggle('slide-show__slide--visible')
+        sichtbaresBild = neuesBild
+    }
+}
+toggleSlide()
